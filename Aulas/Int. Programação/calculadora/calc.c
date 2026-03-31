e9749c371a32e9634e83a076fd7e6465b25d6457
MarqkDi <arielduartewrk@gmail.com>
2026-03-31 08:55:09 -0300

    Primeiros erros lógicos corrigidos

--- Aulas/Int. Programação/calculadora/calc.c
+++ Aulas/Int. Programação/calculadora/calc.c
@@ -10,10 +10,10 @@ int main () {
     printf("Digite o segundo valor: ");
     scanf("%d", &VALOR2);
     //Acho que ficaria melhor se a opção de escolha fosse um numero de 1 a 5, cada um contendo sua operação, a exibição seria apenas "1 - Adição", e fazia isso com cada operador em cada linha,vejo sendo mais prático doque dessa forma digitando o operador
-    printf("\n Digite + pra soma \n Digite - pra subtração \n Digite * pra multiplicação \n Digite / pra divisão \n Digite %% pra módulo \n Insira o oprador: ");
-    scanf("%c", &OP);
+    printf("\n Digite + pra soma \n Digite - pra subtracao \n Digite * pra multiplicacao \n Digite / pra divisao \n Digite %% pra modulo \n Insira o operador: ");
+    scanf(" %c", &OP);
     if (OP != '+' && OP != '-' && OP != '*' && OP != '/' && OP != '%') {
-        printf("Oprador Inválido!");
+        printf("Operador Inválido!");
     }
     else {
         if (OP == '+') {
@@ -29,11 +29,11 @@ int main () {
             printf("O resultado da multiplicação é: %d \n", RESMULT);
         }
         else if (OP == '/') {
-            if (VALOR1 < 1 || VALOR2 < 0) {
+            if (VALOR2 == 0) {
                 printf("Valor inválido (Não há como fazer divisão por 0)");
             }
             else {
-                RESDIV = VALOR1 / VALOR2;
+                RESDIV = (float) VALOR1 / VALOR2;
                 printf("O resultado da divisão é: %f \n", RESDIV);
             }
         }
