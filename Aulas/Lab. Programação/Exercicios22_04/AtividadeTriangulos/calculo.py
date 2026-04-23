2b29fc7f23b373becd1e3c18a2c2557958faa203
MarqkDi <arielduartewrk@gmail.com>
2026-04-23 12:08:27 -0300

    Arrumada função dos triangulos

--- Aulas/Lab. Programação/Exercicios22_04/AtividadeTriangulos/calculo.py
+++ Aulas/Lab. Programação/Exercicios22_04/AtividadeTriangulos/calculo.py
@@ -5,8 +5,8 @@ def clasTriangulo (a):
     if ((a[0] + a[1]) > a[2]):
             if ((a[0] ** 2) + (a[1] ** 2)) == (a[2] ** 2):
                  retangulo = True
-            if (a[0] == a[1]) and (a[0] == a[2]): return 'Equilátero', retangulo
-            elif a[0] == a[1] == a[2]: return 'Isóceles', retangulo
+            if a[0] == a[1] == a[2]: return 'Equilátero', retangulo
+            elif (a[0] == a[1]) or (a[0] == a[2]) or (a[1] == a[2]): return 'Isóceles', retangulo
             elif (a[0] != a[1]) and (a[0] != a[2]) and (a[1] != a[2]): return 'Escaleno', retangulo
     
     else:
